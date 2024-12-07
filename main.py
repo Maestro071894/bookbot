5b6a188b5c1cde7d93970c390a5cd6930dbde3dd
Maestro0794 <bymaestroworkgo94@gmail.com>
2024-12-07 08:05:06 +0300

    update

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = number_of_words(text)
-    print(f"{num_words} words found in the document")
+    char_dict = count_characters(text)
+    print(char_dict)
 
 
 def number_of_words(text):
     words = text.split()
     return len(words)
 
+def count_characters(text):
+    chars = {}
+
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+
+    return chars
 
 def get_book_text(path):
     with open(path) as f:
